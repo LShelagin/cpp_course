34528946d689c51d3b20ff1ef8139d625f566ec4
LShelagin <lshelagin@bk.ru>
2017-09-26 21:31:57 +0400

    fix

---
+++ cpp1/cpp1.2/abc.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+int main()
+{
+    int a = 0;
+    int b = 0;
+    int c = 0;
+    std::cout << "enter three numbers:" << std::endl;
+    std::cin >> a >> b >> c;
+    std::cout << "sum: " << (a + b+ c) << std::endl;
+}
--- cpp1/cpp1.3/cpp1.3.cpp
+++ cpp1/cpp1.3/cpp1.3.cpp
@@ -7,16 +7,22 @@ int main()
     float a = 0;
     float b = 0;
     float c = 0;
-    float x = 0;
     std::scanf("%f %f %f", &a, &b, &c);
     float D = (b * b) - (4 * a * c);
     float x1 = (-b + sqrt(D)) / (2 * a);
     float x2 = (-b - sqrt(D)) / (2 * a);
     if (D > 0)
-        std::printf("x1: %f\n", x1),
-            std::printf("x2: %f\n", x2);
+    {
+        std::printf("x1: %f\n", x1);
+        std::printf("x2: %f\n", x2);
+    }
     else if (D == 0)
-        x = -b / a, std::printf("x: %f\n", x);
+    {
+        float x = -b / a;
+        std::printf("x: %f\n", x);
+    }
     else if (D < 0)
+    {
         std::puts("no solutions");
+    }
 }
